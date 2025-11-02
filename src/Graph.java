065acc29651495f3d08dd891aeed67ff150e1eed
Maryam Assmar <maryam.assmar@edu.univ-fcomte.fr>
2025-11-02 19:48:42 +0100

    functions for getnode, addNode, usesnode, and holds node are added

--- src/Graph.java
+++ src/Graph.java
@@ -27,16 +27,53 @@ public class Graph {
             }
             adjEdList.put(N_set.get(j) ,E_set);
         }
-         }
-
-    public void addEdge(Edge e) {
-        System.out.print("edge added");
     }
+    public int nbNodes() {
+        return adjEdList.size(); }
 
+    public boolean usesNode(int id) {
+        for (Node u : adjEdList.keySet()) if (u.getId() == id) return true;
+        return false;
+    }
+    public boolean usesNode(Node n) {
+        if (n == null) return false;
+        return usesNode(n.getId());
+    }
+    public boolean holdsNode(Node n) {
+        if (n == null) return false;
+        for (Node u : adjEdList.keySet()) if (u.getId() == n.getId() && u.getGraph() == n.getGraph()) return true;
+        return false;
+    }
+    public boolean addNode(int id) {
+        if (usesNode(id)) return false;
+        Node n = new Node(id, this);
+        adjEdList.put(n, new ArrayList<>());
+        return true;
+    }
     public Node getNode(int id) {
-        for (Node u : adjEdList.keySet()) if (u.getId() == id) return u;
+        for (Node u : adjEdList.keySet())
+            if (u.getId() == id)
+                return u;
         return null;
     }
+//    public void addEdge(Node from, Node to, Integer weight) {
+//        if (!usesNode(from.getId()))
+//            addNode(from);
+//        if (!usesNode(to.getId()))
+//            addNode(to);
+//        addEdge(new Edge(from, to, weight));
+//    }
+//    public void addEdge(Node from, Node to) {
+//        addEdge(from, to, null);
+//    }
+    public void addEdge(int fromId, int toId) {
+        addEdge(fromId, toId, null);
+    }
+//    public void addEdge(int fromId, int toId, Integer weight) {
+//        Node from = getNode(fromId); if (from == null) addNode(fromId); from = getNode(fromId);
+//        Node to = getNode(toId); if (to == null) addNode(toId); to = getNode(toId);
+//        addEdge(new Edge(from, to, weight));
+//    }
 
     public List<Node> getSuccessors(Node n) {
         return getSuccessors(n.getId()); }
@@ -50,7 +87,9 @@ public class Graph {
         return true;
     }
 
-    public List<Edge> getOutEdges(Node n) { return getOutEdges(n.getId()); }
+    public List<Edge> getOutEdges(Node n) {
+        return getOutEdges(n.getId());
+    }
     public List<Edge> getOutEdges(int id) {
         Node u = getNode(id);
         if (u == null) return new ArrayList<>();
@@ -58,11 +97,13 @@ public class Graph {
         return new ArrayList<>(out);
     }
 
-    public List<Edge> getInEdges(Node n) { return getInEdges(n.getId()); }
+    public List<Edge> getInEdges(Node n) {
+        return getInEdges(n.getId()); }
     public List<Edge> getInEdges(int id) {
         List<Edge> res = new ArrayList<>();
         for (Map.Entry<Node, List<Edge>> entry : adjEdList.entrySet()) {
-            for (Edge e : entry.getValue()) if (e.to().getId() == id) res.add(e);
+            for (Edge e : entry.getValue())
+                if (e.to().getId() == id) res.add(e);
         }
         return res;
     }
